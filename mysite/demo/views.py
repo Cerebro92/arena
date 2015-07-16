f038c6e11dc2d18cfec69d3dbf76f541bb6362c1
cerebro92 <neeraj.gahlot@delhivery.com>
2015-07-16 12:39:25 +0530

    changed!

--- mysite/demo/views.py
+++ mysite/demo/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render_to_response, render
 from django.views.decorators.csrf import csrf_exempt
 
 def homepage(request):
-	fp=open('/home/delhivery/Test/mysite/demo/templates/homepage.html')
+	fp=open('demo/templates/homepage.html')
 	t=Template(fp.read())
 	fp.close()
 	html=t.render(Context())
@@ -14,7 +14,7 @@ def homepage(request):
 
 
 def login(request):
-	fp=open('/home/delhivery/Test/mysite/demo/templates/login.html')
+	fp=open('demo/templates/login.html')
 	t=Template(fp.read())
 	fp.close()
 	html=t.render(Context())
@@ -27,13 +27,13 @@ def login_view(request):
 		request.session['author_id'] = a.id
 		request.session['author_name'] = a.name
 		request.session['author_email'] = a.email
-		return render(request,'/home/delhivery/Test/mysite/demo/templates/nsession.html/',{"author":a})
+		return render(request,'../../demo/templates/nsession.html/',{"author":a})
 	else:
-		return render(request,'/home/delhivery/Test/mysite/demo/templates/incorrect_login.html/')
+		return render(request,'../../demo/templates/incorrect_login.html/')
 
 
 def signup(request):
-	fp = open('/home/delhivery/Test/mysite/demo/templates/signup.html')
+	fp = open('demo/templates/signup.html')
 	t=Template(fp.read())
 	fp.close()
 	html=t.render(Context())
@@ -46,7 +46,7 @@ def new_session(request):
 	request.session['author_id']=q.id
 	request.session['author_name']=q.name
 	request.session['author_email']=q.email
-	return render(request,'/home/delhivery/Test/mysite/demo/templates/nsession.html',{"author":q})
+	return render(request,'../../demo/templates/nsession.html',{"author":q})
 
 
 def logout(request):
@@ -56,19 +56,19 @@ def logout(request):
 		del request.session['author_email']
 	except KeyError:
 		pass
-	return render(request,'/home/delhivery/Test/mysite/demo/templates/logout.html/')
+	return render(request,'../../demo/templates/logout.html/')
 
 
 def add_view(request):
 	q = Author(id=request.session.get('author_id'), name=request.session.get('author_name'), email=request.session.get('author_email'))
-	return render(request,'/home/delhivery/Test/mysite/demo/templates/user1.html',{"author":q})
+	return render(request,'../../demo/templates/user1.html',{"author":q})
 
 
 def new_article(request):
 	i=request.session.get('author_id')
 	n=request.session.get('author_name')
 	e=request.session.get('author_email')
-	fp = open('/home/delhivery/Test/mysite/demo/templates/add_article.html')
+	fp = open('demo/templates/add_article.html')
 	t = Template(fp.read())
 	fp.close()
 	html=t.render(Context({"author_id":i,"author_name":n,"author_email":e}))
@@ -80,19 +80,30 @@ def view_article(request):
 	a_name=request.session.get('author_name')
 	if aid == None:
 		article = Article.objects.all()
-		return render(request,'/home/delhivery/Test/mysite/demo/templates/view_title.html',{"article_list":article})
+		return render(request,'../../demo/templates/view_title.html',{"article_list":article})
 	else:
 		all_article = Article.objects.all()
 		article = Article.objects.filter(author_name=a_name)
-		return render(request,'/home/delhivery/Test/mysite/demo/templates/view_title_aut.html',{'author_name':a_name,'article_list':article})
+		return render(request,'../../demo/templates/view_title_aut.html',{'author_name':a_name,'article_list':article})
 				
+'''def view_article_content(request):
+	aid = request.session.get('author_id')
+	a_name=request.session.get('author_name')
+	if aid == None:
+		article = Article.objects.all()
+		return render(request,'../../demo/templates/view_article.html',{"article_list":article})
+	else:
+		all_article = Article.objects.all()
+		article = Article.objects.filter(author_name=a_name)
+		return render(request,'../../demo/templates/view_article_aut.html',{'author_name':a_name,'article_list':article})
+'''
 
 
-def suces(request):
+def success(request):
 	name=request.session.get('author_name')
 	at = Article(author_name=name, article_title=request.GET['title'], article_content=request.GET['content'])
 	at.save()
-	fp = open('/home/delhivery/Test/mysite/demo/templates/succes.html')
+	fp = open('demo/templates/success.html')
 	t = Template(fp.read)
 	fp.close()
 	html = t.render(Context({"title":at.article_title}))
